9e8016c742ecc3c9a64c449c9540616cdce538ba
Mader <madergabriel2@gmail.com>
2024-07-08 23:19:32 -0300

    :sparkles: feat: Adiciona arquivo de constantes

---
+++ backend/src/constants/index.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_PAGE = '1';
+export const DEFAULT_LIMIT = '10';
+export const MAX_DAILY_APPOINTMENTS = 20;
+export const MAX_HOURLY_APPOINTMENTS = 2;
+export const VALID_STATUSES = ['PENDING', 'FINISHED', 'CANCELED']
