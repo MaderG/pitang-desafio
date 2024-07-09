b33ccd4791072f91abcf2773b83a40cc86aaaf1d
Mader <madergabriel2@gmail.com>
2024-07-09 16:05:30 -0300

    :recycle: refactor: Remove funcoes inutilizadas

--- backend/src/utils/dateUtils.ts
+++ backend/src/utils/dateUtils.ts
@@ -1,34 +1,7 @@
-import { addHours, endOfDay, format, isBefore, isValid, parseISO, setHours, setMinutes, startOfDay } from "date-fns";
+import {parseISO, setHours, setMinutes } from "date-fns";
 
 export const constructDateTime = (date: string, time: string): Date => {
   const datePart = parseISO(date);
   const [hour, minute] = time.split(':').map(Number);
   return setMinutes(setHours(datePart, hour), minute);
 };
-
-export function parseAndValidateDate(dateString: string): Date | null {
-  if (!dateString || !isValid(parseISO(dateString))) {
-    return null;
-  }
-  return parseISO(dateString);
-}
-
-export function getStartAndEndOfDay(date: Date): { start: Date; end: Date } {
-  return {
-    start: startOfDay(date),
-    end: endOfDay(date),
-  };
-}
-
-export function generateTimeSlots(startHour: number, endHour: number, currentDate: Date): string[] {
-  const slots = [];
-  let currentTime = setHours(currentDate, startHour);
-  const endTime = setHours(currentDate, endHour);
-
-  while (isBefore(currentTime, endTime)) {
-    slots.push(format(currentTime, 'HH:mm'));
-    currentTime = addHours(currentTime, 1);
-  }
-
-  return slots;
-}
