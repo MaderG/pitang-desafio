d3ea331f2c425d8812eeeb74540ff0f99afb85be
Mader <madergabriel2@gmail.com>
2024-07-09 15:31:25 -0300

    :recycle: refactor: Adiciona mais checagens e erros

--- backend/src/services/updateAppointment.service.ts
+++ backend/src/services/updateAppointment.service.ts
@@ -4,9 +4,27 @@ import { VALID_STATUSES } from '../constants'
 import { InvalidStatusError } from '../errors/InvalidStatusError'
 import { mapStatusToEnglish } from '../utils/statusUtils'
 import { AppointmentNotExistsError } from '../errors/AppointmentNotExistsError'
+import { MissingParametersError } from '../errors/MissingParametersError'
+import { UnableToUpdateError } from '../errors/UnableToUpdateError'
 
 export class UpdateAppointmentService {
   async updateAppointment(id: string, status: string): Promise<Appointment> {
+    if (!id) {
+      throw new MissingParametersError('É necessário informar o id do agendamento')
+    }
+    
+    if (!status) {
+      throw new MissingParametersError('É necessário informar o status')
+    }
+    
+    const existingAppointment = await prisma.appointment.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!existingAppointment) {
+      throw new AppointmentNotExistsError('Agendamento não encontrado');
+    }
+
     const newStatus = await this.validateStatus(status)
     try {
       const appointment = await prisma.appointment.update({
@@ -19,15 +37,12 @@ export class UpdateAppointmentService {
       })
 
       return appointment
-    } catch (error) {
-      throw new AppointmentNotExistsError('Agendamento não encontrado')
+    } catch (err) {
+      throw new UnableToUpdateError('Não foi possível atualizar o agendamento')
     }
   }
 
   private async validateStatus(status: string): Promise<string> {
-    if (!status) {
-      throw new InvalidStatusError('É necessário informar o status')
-    }
     const translatedStatus = mapStatusToEnglish(status)
     const isValidStatus = VALID_STATUSES.includes(translatedStatus)
     if (!isValidStatus) {
