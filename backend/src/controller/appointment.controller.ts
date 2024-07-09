b97075970757ae686156d26b89d2b234c66b6a6f
Mader <madergabriel2@gmail.com>
2024-07-08 23:38:21 -0300

    :recycle: refactor: Refatora metodo update

--- backend/src/controller/appointment.controller.ts
+++ backend/src/controller/appointment.controller.ts
@@ -1,31 +1,49 @@
 import { Request, Response } from 'express'
 import { prisma } from '../lib/prisma'
-import {AppointmentSchema} from '../zod'
+import { AppointmentSchema } from '../zod'
 import { AppointmentQuery } from '../types/AppointmentQuery'
 import { constructDateTime } from '../utils/dateUtils'
 import { BookingBoundsError } from '../errors/BookingBoundsError'
 import { AppointmentInput } from '../types/AppointmentInput'
-import { addHours, endOfDay, format, isBefore, isValid, parseISO, setHours, startOfDay } from 'date-fns'
+import {
+  addHours,
+  endOfDay,
+  format,
+  isBefore,
+  isValid,
+  parseISO,
+  setHours,
+  startOfDay,
+} from 'date-fns'
 import { PastDateError } from '../errors/PastDateError'
 import { AlreadyBookedError } from '../errors/AlreadyBookedError'
 import { createAppointmentService } from '../services/createAppointment.service'
 import { listAppointmentService } from '../services/listAppointment.service'
 import { InvalidDateError } from '../errors/InvalidDateError'
 import { InvalidStatusError } from '../errors/InvalidStatusError'
+import { updateAppointmentService } from '../services/updateAppointment.service'
+import { AppointmentNotExistsError } from '../errors/AppointmentNotExistsError'
 
 export default class AppointmentController {
   async create(req: Request, res: Response): Promise<Response> {
     try {
-      const inputData: AppointmentInput = AppointmentSchema.parse(req.body);
-      const dateObj = constructDateTime(inputData.date, inputData.time);
-      
-      const appointment = await createAppointmentService.createAppointment(inputData, dateObj);
-      return res.status(201).json(appointment);
+      const inputData: AppointmentInput = AppointmentSchema.parse(req.body)
+      const dateObj = constructDateTime(inputData.date, inputData.time)
+
+      const appointment = await createAppointmentService.createAppointment(
+        inputData,
+        dateObj,
+      )
+      return res.status(201).json(appointment)
     } catch (err) {
-      if (err instanceof BookingBoundsError || err instanceof PastDateError || err instanceof AlreadyBookedError) {
-        return res.status(400).json({ error: err.message });
+      if (
+        err instanceof BookingBoundsError ||
+        err instanceof PastDateError ||
+        err instanceof AlreadyBookedError
+      ) {
+        return res.status(400).json({ error: err.message })
       }
-      return res.status(500).json({ error: 'Internal Server Error' });
+      return res.status(500).json({ error: 'Internal Server Error' })
     }
   }
 
@@ -37,45 +55,54 @@ export default class AppointmentController {
         date,
         status,
         sortBy = 'date',
-        order = 'asc'
-    } = req.query as Partial<AppointmentQuery>;
+        order = 'asc',
+      } = req.query as Partial<AppointmentQuery>
 
-    const query: AppointmentQuery = {
-      page,
-      limit,
-      date,
-      status,
-      sortBy,
-      order
-  };
-
-      const { totalPages, appointments } = await listAppointmentService.listAppointments(query);
-
-      return res.status(200).json({ totalPages, appointments });
-    } catch (err) {
-      if (err instanceof InvalidDateError || err instanceof InvalidStatusError) {
-        console.log(err.message)
-        return res.status(400).json({ error: err.message });
+      const query: AppointmentQuery = {
+        page,
+        limit,
+        date,
+        status,
+        sortBy,
+        order,
       }
-      console.error('Error retrieving appointments:', err);
-      return res.status(500).json({ error: 'Internal Server Error' });
+
+      const { totalPages, appointments } =
+        await listAppointmentService.listAppointments(query)
+
+      return res.status(200).json({ totalPages, appointments })
+    } catch (err) {
+      if (
+        err instanceof InvalidDateError ||
+        err instanceof InvalidStatusError
+      ) {
+        console.log(err.message)
+        return res.status(400).json({ error: err.message })
+      }
+      console.error('Error retrieving appointments:', err)
+      return res.status(500).json({ error: 'Internal Server Error' })
     }
   }
 
   async update(req: Request, res: Response) {
-    const { id } = req.params
-    const { status } = req.body
+    try {
+      const { id } = req.params
+      const { status } = req.body
 
-    const appointment = await prisma.appointment.update({
-      where: {
-        id: Number(id),
-      },
-      data: {
+      const appointment = await updateAppointmentService.updateAppointment(
+        id,
         status,
-      },
-    })
-
-    return res.json(appointment)
+      )
+      return res.json(appointment)
+    } catch (err) {
+      if (
+        err instanceof InvalidStatusError ||
+        err instanceof AppointmentNotExistsError
+      ) {
+        return res.status(400).json({ error: err.message })
+      }
+      return res.status(500).json({ error: 'Internal Server Error' })
+    }
   }
 
   async listAvailableDays(req: Request, res: Response) {
@@ -85,10 +112,10 @@ export default class AppointmentController {
           date: true,
         },
       })
-      
+
       const dates = appointments.map((appointment) =>
-        format(startOfDay(new Date(appointment.date)), 'yyyy-MM-dd')
-      );
+        format(startOfDay(new Date(appointment.date)), 'yyyy-MM-dd'),
+      )
       const uniqueDates = [...new Set(dates)]
       return res.json(uniqueDates)
     } catch (err) {
@@ -100,14 +127,16 @@ export default class AppointmentController {
 
   async listAvailableTimes(req: Request, res: Response) {
     try {
-      const { date } = req.query;
+      const { date } = req.query
       if (!date || !isValid(parseISO(date as string))) {
-        return res.status(400).json({ error: 'Missing or invalid date query parameter' });
+        return res
+          .status(400)
+          .json({ error: 'Missing or invalid date query parameter' })
       }
-  
-      const dayStart = startOfDay(parseISO(date as string));
-      const dayEnd = endOfDay(parseISO(date as string));
-  
+
+      const dayStart = startOfDay(parseISO(date as string))
+      const dayEnd = endOfDay(parseISO(date as string))
+
       const appointments = await prisma.appointment.findMany({
         where: {
           date: {
@@ -118,30 +147,30 @@ export default class AppointmentController {
         select: {
           date: true,
         },
-      });
-  
-      const timeCounts: Record<string, number> = {};
-      appointments.forEach(appointment => {
-        const time = format(appointment.date, 'HH:mm');
-        timeCounts[time] = (timeCounts[time] || 0) + 1;
-      });
-  
-      const availableTimes: string[] = [];
-      let currentTime = setHours(dayStart, 8);
-      const endTime = setHours(dayStart, 18);
-  
+      })
+
+      const timeCounts: Record<string, number> = {}
+      appointments.forEach((appointment) => {
+        const time = format(appointment.date, 'HH:mm')
+        timeCounts[time] = (timeCounts[time] || 0) + 1
+      })
+
+      const availableTimes: string[] = []
+      let currentTime = setHours(dayStart, 8)
+      const endTime = setHours(dayStart, 18)
+
       while (isBefore(currentTime, endTime)) {
-        const formattedHour = format(currentTime, 'HH:mm');
+        const formattedHour = format(currentTime, 'HH:mm')
         if (!timeCounts[formattedHour] || timeCounts[formattedHour] < 2) {
-          availableTimes.push(formattedHour);
+          availableTimes.push(formattedHour)
         }
-        currentTime = addHours(currentTime, 1);
+        currentTime = addHours(currentTime, 1)
       }
-  
-      return res.json(availableTimes);
+
+      return res.json(availableTimes)
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(500).json({ error: err.message });
+        return res.status(500).json({ error: err.message })
       }
     }
   }
