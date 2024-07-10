cda99c256b83beeb4de755b47f83158f5fcba29f
Mader <madergabriel2@gmail.com>
2024-07-10 17:46:16 -0300

    :recycle: refctor: Adiciona limite de 20 caracteres para o nome

--- frontend/src/zod/index.ts
+++ frontend/src/zod/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 const VaccineAppointmentSchema = z.object({
-  name: z.string().min(1, "Você precisa informar um nome"),
+  name: z.string().min(1, "Você precisa informar um nome").max(20, 'O nome deve ter no máximo 20 caracteres'),
   birthDate: z.date({ message: "Informe uma data de nascimento" }),
   date: z.date({ message: "Informe uma data de vacinação" }),
   time: z.string({ message: "Informe um horário de vacinação" }),
