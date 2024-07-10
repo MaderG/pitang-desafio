37604885bf7cae2b41164a7fa0d27d4fc28331a6
Mader <madergabriel2@gmail.com>
2024-07-10 19:24:27 -0300

    :label: feat: Adiciona tipgens para as constants

--- backend/src/utils/constants/index.ts
+++ backend/src/utils/constants/index.ts
@@ -1,8 +1,8 @@
-export const DEFAULT_PAGE = '1';
-export const DEFAULT_LIMIT = '8';
-export const MAX_DAILY_APPOINTMENTS = 20;
-export const MAX_HOURLY_APPOINTMENTS = 2;
-export const VALID_STATUSES = ['PENDING', 'FINISHED', 'CANCELED']
-export const VALID_SORT_BY = ['date', 'time', 'name']
-export const TIME_FORMAT = 'HH:mm'
-export const DATE_FORMAT = 'yyyy-MM-dd'
+export const DEFAULT_PAGE:string = '1';
+export const DEFAULT_LIMIT:string = '8';
+export const MAX_DAILY_APPOINTMENTS:number = 20;
+export const MAX_HOURLY_APPOINTMENTS:number = 2;
+export const VALID_STATUSES: string[] = ['PENDING', 'FINISHED', 'CANCELED']
+export const VALID_SORT_BY: string[] = ['date', 'time', 'name']
+export const TIME_FORMAT: string = 'HH:mm'
+export const DATE_FORMAT: string = 'yyyy-MM-dd'
