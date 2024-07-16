643892f86173491b7ccd64de3d30fe2029cadb1a
Mader <madergabriel2@gmail.com>
2024-07-15 22:58:27 -0300

    :rocket: chore: release 1.0.4

--- backend/swagger/swaggerConfig.ts
+++ backend/swagger/swaggerConfig.ts
@@ -6,7 +6,7 @@ const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
     title: 'API para Agendamentos de Vacinas',
-    version: '1.0.3',
+    version: '1.0.4',
     description:
       'Esta é uma API desenvolvida para o gerenciamento de agendamentos de vacinas.',
     contact: {
