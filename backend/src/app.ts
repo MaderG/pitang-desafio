a972820bcc5e7b7766dfd305701b381520a179c9
Mader <madergabriel2@gmail.com>
2024-07-15 21:52:34 -0300

    :sparkles: feat: Adiciona Documentação com swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,9 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express'
+
+import swaggerSpec from '../swagger/swaggerConfig'
 import appointmentRouter from './routes/appointment.routes'
 
 export const app = express()
@@ -12,5 +15,9 @@ export const app = express()
 app.use(helmet())
 app.use(morgan('dev'))
 app.use(cors())
+
 app.use(express.json())
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
+
 app.use(appointmentRouter)
---
+++ backend/swagger/swaggerConfig.ts
@@ -0,0 +1,47 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+import { env } from '../src/env/env'
+import { components } from './swaggerComponents'
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'API para Agendamentos de Vacinas',
+    version: '1.0.3',
+    description:
+      'Esta é uma API desenvolvida para o gerenciamento de agendamentos de vacinas.',
+    contact: {
+      name: 'Mader Gabriel',
+      email: 'madergabriel2@gmail.com',
+    },
+  },
+  servers: [
+    {
+      url: `http://localhost:${env.PORT}`,
+      description: 'Servidor Local',
+    },
+  ],
+  components: {
+    ...components,
+    securitySchemes: {
+      bearerAuth: {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+    },
+  },
+  security: [
+    {
+      bearerAuth: [],
+    },
+  ],
+}
+
+const options = {
+  swaggerDefinition,
+  apis: ['./swagger/swaggerDocumentation.ts'],
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+export default swaggerSpec
