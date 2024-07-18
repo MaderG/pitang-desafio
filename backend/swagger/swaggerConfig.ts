1b14c995bde500cb84f7f8bbf92df80c381a913c
Mader <madergabriel2@gmail.com>
2024-07-18 20:15:54 -0300

    :books: docs: Altera documentação para 1.0.10

--- backend/swagger/swaggerConfig.ts
+++ backend/swagger/swaggerConfig.ts
@@ -6,7 +6,7 @@ const swaggerDefinition = {
   openapi: '3.0.0',
   info: {
     title: 'API para Agendamentos de Vacinas',
-    version: '1.0.9',
+    version: '1.0.10',
     description:
       'Esta é uma API desenvolvida para o gerenciamento de agendamentos de vacinas.',
     contact: {
